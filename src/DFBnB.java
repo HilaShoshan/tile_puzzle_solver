20262d13e93f620ea667a7b764718f0d58f05273
Hila Shoshan <hilashoshan0605@gmail.com>
2021-05-10 21:27:49 +0300

    DFBnB working on input1

--- src/DFBnB.java
+++ src/DFBnB.java
@@ -29,24 +29,27 @@ public class DFBnB implements Algorithm {
         while (!L.isEmpty()) {
             n = L.pop();  // remove front and saves it as Node n
             if (n.isOUT()) {
-                //System.out.println("out");
                 H.remove(n.toString());
             } else {
                 n.setOUT(true);
                 L.push(n);
                 findChildren(n);  // after this function, the "children" ArrayList contains all the children of n, and sorted by children's f values
-                for (int i = 0; i < children.size(); i++) {
-                    g = children.get(i);
+                Iterator<Node> itr = children.iterator();
+                while (itr.hasNext()) {
+                    g = itr.next();
                     boolean contains_g = H.containsKey(g.toString());
                     if (f(g) >= t) {
-                        for (int j = children.size()-1 ; j >= i  ; j--)
-                            children.remove(j);  // remove g and all the nodes after it from children
+                        itr.remove();
+                        while (itr.hasNext()) {
+                            itr.next();
+                            itr.remove();  // remove g and all the nodes after it from children
+                        }
                     } else if (contains_g) {
                         Node g_twin = H.get(g.toString());
                         if (g_twin.isOUT())  // If H contains g’=g and g’ is marked as "out"
-                            children.remove(g_twin);  // remove g from children
+                            itr.remove();  // remove g from children
                         else {  // If H contains g’=g and g’ is not marked as "out"
-                            if (f(g_twin) <= f(g)) children.remove(g_twin);
+                            if (f(g_twin) <= f(g)) itr.remove();
                             else {
                                 L.remove(g_twin);
                                 H.remove(g.toString());
@@ -55,9 +58,11 @@ public class DFBnB implements Algorithm {
                     } else if (HelperFunctions.isGoal(goal_matrix, g.getBoard())) {  // if we reached here, f(g) < t
                         t = f(g);
                         state = g;  // like setting the "result" to path(g)
-                        System.out.println("found goal, iteration "+iteration);
-                        for (int j = children.size()-1 ; j >= i  ; j--)
-                            children.remove(j);  // remove g and all the nodes after it from children
+                        itr.remove();
+                        while (itr.hasNext()) {
+                            itr.next();
+                            itr.remove();  // remove g and all the nodes after it from children
+                        }
                     }
                 }
                 // insert children list in a reverse order to L and H
